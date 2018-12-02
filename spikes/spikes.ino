176e7d8a8fa1cec7d1e8ef392343ca80129c93d3
simondlevy <simon.d.levy@gmail.com>
2018-12-02 15:46:38 -0500

    Added license info

--- spikes/spikes.ino
+++ spikes/spikes.ino
@@ -1,3 +1,23 @@
+/*
+Simple spike generator
+
+Copyright (C) Simon D. Levy 2018
+
+This file is part of csci252-examples.
+
+csci252-examples is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+This code is distributed in the hope that it will be useful,     
+but WITHOUT ANY WARRANTY without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License 
+along with this code.  If not, see <http:#www.gnu.org/licenses/>.
+*/
+
 static const uint8_t  INPUT_PIN  = A0;
 static const uint8_t  OUTPUT_PIN = A1;
 
